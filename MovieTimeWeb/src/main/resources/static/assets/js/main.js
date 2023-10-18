888a0fe0011234cbaef3619f9b21c2a985cbe6af
Quang Vinh <121350118+Tzinh513@users.noreply.github.com>
2023-10-18 15:22:51 +0700

    update

--- MovieTimeWeb/src/main/resources/static/assets/js/main.js
+++ MovieTimeWeb/src/main/resources/static/assets/js/main.js
@@ -8,6 +8,9 @@
 // }
 
 
+
+
+// ==========================SLIDER IMAGE===================
 const initSlider = () => {
     const imageList = document.querySelector(".slider-wrapper .image-list");
     const slideButtons = document.querySelectorAll(".slider-wrapper .slide-button");
